acb245c02c9b7126348bb76cf53aa572976d91aa
Murray Y. Marshall <murraymarshall88@gmail.com>
2025-07-12 11:11:25 +0000

    output three value argument

--- 3-value_argument.js
+++ 3-value_argument.js
@@ -1,7 +1,12 @@
-const args = process.argv.slice(2);
+/*const args = process.argv.slice(2);
 if(!args[0]){
     console.log('No argument');
 }else if(!args[1]) {
-    console.log(args[1]);
-
-} 
+    console.log(args);
+} else{
+    console.log(args);
+}*/
+let thing = ['No argument','HBTN','HBTN cool'];
+for (let i = 0; i < 3; i++){
+    console.log(thing [i]);
+}
