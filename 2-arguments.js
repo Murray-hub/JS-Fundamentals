91ec7093e73c8f69fd0fbc6d771a326965b5130b
Murray Y. Marshall <murraymarshall88@gmail.com>
2025-07-12 01:07:11 +0000

    decleare two function

---
+++ 2-arguments.js
@@ -0,0 +1,10 @@
+function myFunction(num1, num2){
+     console.log('No argument');
+}
+
+myFunction();
+function introduce(name, age){
+    console.log ('argument found');
+   
+}
+introduce('Murray', 19);
