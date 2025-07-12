3ddc98cb5ec04a161f2feb8fa62b8e0aa48a84a0
Murray Y. Marshall <murraymarshall88@gmail.com>
2025-07-12 10:17:07 +0000

    create a conditional to print argument when it pass using node

--- 3-value_argument.js
+++ 3-value_argument.js
@@ -2,7 +2,7 @@ const args = process.argv.slice(2);
 if(!args[0]){
     console.log('No argument');
 }else if(!args[1]) {
-    console.log('one argument "HBNT"');
+    console.log(args);
 } else{
-    console.log('2 argument: "HBNT cool"');
+    console.log(args);
 }
