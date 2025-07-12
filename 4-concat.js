2dbcf79be7ab02266e88d7f6d5b78b21fa7afc94
Murray Y. Marshall <murraymarshall88@gmail.com>
2025-07-12 18:41:33 +0000

    create a conditional to print out three different argument base on the argument pass

--- 4-concat.js
+++ 4-concat.js
@@ -1,7 +1,7 @@
 let john ;
 const names = process.argv.slice(2);
 if (!names[0]){
-    console.log (`${john}is ${john}`);
+    console.log (`${john} is ${john}`);
 }else if (!names[1]){
 console.log(`HBTN is ${john}`);
 }else{
